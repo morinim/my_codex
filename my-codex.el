4dc30c7ac46ca9277b988c3de93b773466b0dffe
Manlio Morini <morinim@gmail.com>
2026-06-11 20:39:28 +0200

    chore: bump package version to 0.9.10
    Update the package header version for the next release.

--- my-codex.el
+++ my-codex.el
@@ -5,7 +5,7 @@
 ;; Author: Manlio Morini
 ;; Keywords: tools, convenience
 ;; URL: https://github.com/morinim/my_codex
-;; Version: 0.9.9
+;; Version: 0.9.10
 ;; Package-Requires: ((emacs "29.1") (vterm "0") (transient "0"))
 
 ;; This file is not part of GNU Emacs.
